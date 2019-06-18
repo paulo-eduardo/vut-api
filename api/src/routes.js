1075c330611ebdf32ec5b5bd1602bf91ba7c4fc4
Paulo Eduardo <paulo.es@live.com>
2019-06-18 20:22:06 -0300

    fixing first test

--- api/src/routes.js
+++ api/src/routes.js
@@ -2,4 +2,8 @@ const express = require('express')
 
 const routes = new express.Router()
 
+routes.get('/', (req, res) => {
+  return res.status(200).json({ message: 'Server is up' })
+})
+
 module.exports = routes
