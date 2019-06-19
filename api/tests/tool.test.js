3a5dc73a0aa127fd8cef6074dfbd2db737ae63c6
Paulo Eduardo <paulo.es@live.com>
2019-06-18 21:55:23 -0300

    fixing text title and adding new test

--- api/tests/tool.test.js
+++ api/tests/tool.test.js
@@ -24,7 +24,7 @@ describe('Tools API Tests', function () {
     token = response.body.access_token
   })
 
-  it('GET /v1/tools/ returns an array of movies', async () => {
+  it('GET /v1/tools/ returns an array of tool', async () => {
     const response = await request(server)
       .get('/v1/tools/')
       .set({
@@ -36,50 +36,21 @@ describe('Tools API Tests', function () {
     expect(response.status).to.equal(200)
     expect(response.body).to.be.an.instanceof(Array)
   })
+
+  it('POST /v1/tools/ create and return an tool', async () => {
+    const response = await request(server)
+      .post('/v1/tools/')
+      .set({
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      })
+      .send({
+        title: 'Hello, world!',
+        link: 'Hello, world!',
+        description: 'Hello, world!',
+        tags: ['Hello, world!']
+      })
+    expect(response.status).to.equal(201)
+  })
 })
-
-// describe('Tools API Tests', () => {
-//   let token;
-
-//   before(async () => {
-//     // runs before all tests
-//     // gets a new token for the requests
-
-//     await truncate.truncateUser();
-
-//     const response = await request(server)
-//       .post('/v1/register/')
-//       .send({ name: 'Nay', email: 'nay@linda.com', password: '123456' });
-//     token = response.body.access_token;
-//   });
-
-//   it('GET /v1/tools/ returns an array of movies', async () => {
-//     const response = await request(server)
-//       .get('/v1/tools/')
-//       .set({
-//         Authorization: `Bearer ${token}`,
-//         Accept: 'application/json',
-//         'Content-Type': 'application/json',
-//       });
-
-//     expect(response.status).to.equal(200);
-//     expect(response.body).to.be.an.instanceof(Array);
-//   });
-
-//   it('POST /v1/tools/ create and return an tool', async () => {
-//     const response = await request(server)
-//       .post('/v1/tools/')
-//       .set({
-//         Authorization: `Bearer ${token}`,
-//         Accept: 'application/json',
-//         'Content-Type': 'application/json',
-//       })
-//       .send({
-//         title: 'Hello, world!',
-//         link: 'Hello, world!',
-//         description: 'Hello, world!',
-//         tags: ['Hello, world!'],
-//       });
-//     expect(response.status).to.equal(201);
-//   });
-// });
